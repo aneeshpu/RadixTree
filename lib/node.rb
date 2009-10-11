52d8b9180ad690d4393f6d3813fcdce371142f4d
aneeshpu <aneeshpu@hogwarts.(none)>
2009-10-11 14:02:14 +0530

    Yen can search...yay!

--- lib/node.rb
+++ lib/node.rb
@@ -1,4 +1,5 @@
 require 'string'
+require 'not_my_child_error'
 
 module RadixTree
   class Node
@@ -20,46 +21,69 @@ module RadixTree
     end
 
     def add child
-      return false if !is_really_my_child? child
+      raise NotMyChildError, "#{child} is not a child of #{self}" if !is_really_my_child? child
+      
       add_child child
     end
 
     private
     def add_child child
+      
       @children.each do |existing_child|
-        if existing_child.is_sibling?(child,self)
-          fork_a_new_child_and_add_to_children existing_child, child
+
+        if existing_child.is_really_my_child? child
+          existing_child.add child
           return
         end
+
+        if existing_child.is_sibling_of_new_child_to_be_added? child,self
+          fork_a_new_parent_and_add_to_children existing_child, child
+          return
+        end
+        
       end
-      @children << child
-      true
+
+      accept_as_own_child child
     end
 
-    def fork_a_new_child_and_add_to_children existing_child,new_child
+    def fork_a_new_parent_and_add_to_children existing_child,new_child
       new_parent = existing_child.fork_a_new_parent_and_add new_child
+      
+      disown_existing_child existing_child
+      
+      add_new_child new_parent
+    end
+
+    private
+    def add_new_child new_child
+      @children << new_child
+    end
+
+    private
+    def disown_existing_child existing_child
       @children.delete existing_child
-      @children << new_parent
+      self
     end
 
     protected
-    def is_sibling? other,parent
-      common_beginning_name=other.name.common_beginning(@name)
+    def is_sibling_of_new_child_to_be_added? other,parent
+      common_beginning_name = common_beginning_in_name other.name
       return false if common_beginning_name.nil?
+      
       common_beginning_name.chomp!(separator=' ')
       new_found_parent = Node.new common_beginning_name
       result = !(new_found_parent == parent)
       result
     end
 
-    def add_blindly child
+    def accept_as_own_child child
       @children << child
     end
 
     def fork_a_new_parent_and_add child
       new_parent=Node.new @name.common_beginning child.name
-      new_parent.add_blindly child
-      new_parent.add_blindly self
+      new_parent.accept_as_own_child child
+      new_parent.accept_as_own_child self
       new_parent
     end
 
@@ -67,8 +91,18 @@ module RadixTree
       child.name.starts_with @name
     end
 
+    private
+    def common_beginning_in_name other_name
+      return @name.common_beginning other_name
+    end
+
+    def names_have_a_common_beginning? other_name
+      return !@name.common_beginning(other_name).nil?
+    end
+
     public
     def == other
+      return false if other == nil
       @name == other.name
     end
 
@@ -78,5 +112,42 @@ module RadixTree
 
     protected
     attr_reader :name
+
+    public
+    def tree_string
+      tree_string = @name
+      @children.each { |child| 
+          tree_string = "#{tree_string}\n|\n|__" << child.tree_string
+        }
+      tree_string
+    end
+
+    def has_child(purportedChild)
+      @children.include? purportedChild
+    end
+
+    def find_child purportedChild
+      @children.find {|child| child == purportedChild}
+    end
+
+    def search_for node_name
+
+      if node_name.starts_with @name
+        return add_me_and_my_descendants
+      end
+
+    end
+
+    protected
+    def add_me_and_my_descendants
+      me_and_my_descendants = [self]
+
+      @children.each do |child|
+        me_and_my_descendants.concat(child.add_me_and_my_descendants)
+      end
+
+      return me_and_my_descendants
+    end
   end
 end
+
--- test/node_test.rb
+++ test/node_test.rb
@@ -20,13 +20,79 @@ module RadixTree
 
     def test_should_not_add_a_wrong_child
       london = Node.new "London"
-      assert !london.add(Node.new "Manchester")
-      assert london.has_no_children?
+      begin
+        assert !london.add(Node.new "Manchester")
+        fail "#{london} should not have allowed Manchester to be added as a child"
+      rescue NotMyChildError
+      ensure
+        assert london.has_no_children?
+      end     
+      
     end
 
     def test_should_produce_readable_representation_of_itself
       london = Node.new "London"
       assert("London" == london.to_s)
+    end
+
+#    def test_should_produce_readable_representation_of_tree
+#      london = Node.new "London"
+#
+#      london_bridge = Node.new "London Bridge"
+#      london_bridge.add Node.new "London Bridge Falling"
+#
+#      puts london_bridge.tree_string
+#
+#      london.add london_bridge
+#      london.add Node.new "London Cannon Street"
+#
+#      puts london.tree_string
+#
+#      assert "London\t\n|\n|__London Bridge\n|\n|__London Cannon Street" == london.tree_string
+#    end
+
+    def test_should_identify_already_added_child
+
+      london = Node.new "London"
+      london.add Node.new "London Bridge"
+
+      assert london.has_child Node.new 'London Bridge'
+    end
+
+
+    def test_should_automatically_fork_child
+      london = Node.new "London"
+
+      london.add Node.new "London Bridge"
+      london.add Node.new "London Blackfairs"
+
+      assert london.has_child Node.new "London B"
+    end
+
+    def test_should_be_unequal_if_other_is_nil
+      assert Node.new "London" != nil
+    end
+
+    def test_should_return_child
+      london = Node.new "London"
+      london.add Node.new "London Bridge"
+
+      london_bridge = london.find_child Node.new "London Bridge"
+      puts london_bridge
+
+      assert london_bridge != nil
+    end
+
+    def test_auto_forked_child_should_contain_children_that_previously_existed_at_its_place
+      london = Node.new "London"
+      london.add Node.new "London Bridge"
+      london.add Node.new "London Blackfairs"
+
+      london_b = london.find_child Node.new "London B"
+      
+      assert_equal 2, london_b.number_of_children
+      assert london_b.find_child(Node.new 'London Bridge') != nil
+      assert london_b.find_child(Node.new 'London Blackfairs') != nil
     end
 
     def test_should_split_and_fork_children_when_siblings_are_detected
@@ -44,6 +110,40 @@ module RadixTree
 
       london.add Node.new "London Blackfairs"
       assert_equal 3, london.number_of_children, "London should have forked a London B child with two of its own children London Bridge and London Blackfairs"
+    end
+
+    def test_should_add_grand_child
+
+      london = Node.new "London"
+      london_b = Node.new "London B"
+      london.add london_b
+      london.add Node.new "London Bridge"
+
+      assert london_b.has_child Node.new "London Bridge"
+    end
+
+    def test_should_own_child_even_when_grand_child_is_born
+      london = Node.new "London"
+      london_b = Node.new "London B"
+      london.add london_b
+      london.add Node.new "London Bridge"
+
+      assert london.has_child Node.new "London B"
+    end
+
+    def test_search_for_parent_should_yield_parent_and_all_descendants
+
+      london = Node.new "London"
+      london.add Node.new "London B"
+      london.add Node.new "London Bridge"
+
+      results = london.search_for "London"
+      results.each{|result| puts "found:#{result}"}
+
+      assert_equal 3, results.size, "Expected only three elements to be found"
+      assert_equal Node.new("London"), results[0], "Expected first element to be London"
+      assert_equal Node.new("London B"), results[1], "Expected first element to be London"
+      assert_equal Node.new("London Bridge"), results[2], "Expected first element to be London"
     end
   end
 end
